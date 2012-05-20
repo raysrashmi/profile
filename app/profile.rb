8fe7aa879c1be4cefae38af062e3b79d0b2b95b3
Arun Agrawal <arunagw@gmail.com>
2012-05-20 14:43:35 +0530

    some urls for image

--- app/profile.rb
+++ app/profile.rb
@@ -6,8 +6,8 @@ class Profile
   end
   All = [
          Profile.new('Rashmi Yadav','rays.jpg'),
-         Profile.new('Arun Agrawal', 'arun.jpg'),
-         Profile.new('Karan Arora', 'karan.jpg'),
+         Profile.new('Arun Agrawal', 'http://mustachify.me/?src=https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/473379_10150749120579954_560559953_11735450_888061565_o.jpg'),
+         Profile.new('Karan Arora', 'http://mustachify.me/?src=https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/327253_2395817469805_1682037004_1547810_280897014_o.jpg'),
          Profile.new('Richa Saksena', 'richa.jpg')
        ]
 end
